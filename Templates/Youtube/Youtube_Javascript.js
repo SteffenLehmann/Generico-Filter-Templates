08271f81e731b940e2014e5b4575d389691f0d09
SteffenLehmann <slehma15@student.aau.dk>
2023-08-01 13:39:17 +0200

    Added header link to youtube template

--- Templates/Youtube/Youtube_Javascript.js
+++ Templates/Youtube/Youtube_Javascript.js
@@ -4,16 +4,20 @@ const nameForSummary = '@@Name: The name of the button containing the YouTube vi
 const YouTubeURL = '@@YouTube URL: Copy in the link/URL of the video you want to embed@@'; // user input
 
 // getting the elements from the HTML
-const fullscreenIframeContainer = document.getElementById('iframeContainer'); 
-//const fullscreenButton = document.getElementById('fullscreenButton');
-//const exitFullscreenButton = document.getElementById('exitFullscreenButton');
 const details = document.getElementById('Details'+@@AUTOID@@);
 const detailsButton = document.getElementById('detailsButton');
+const headerLink = document.getElementById('ShareLinkHeader'+@@AUTOID@@);
 
 // function calls to create the temlate on moodle
 createNameForSummary(nameForSummary);
-console.log('YouTubeURL ' + YouTubeURL);
+assignHeaderLinks(YouTubeURL);
 onLoad(YouTubeURL);
+
+
+// function to assign the header links
+function assignHeaderLinks(url) {
+  headerLink.href = ""+ url;
+}
 
 //creates the name for the template
 function createNameForSummary(name) {
@@ -40,17 +44,16 @@ function setSameSiteAttribute(sameSiteValue) {
 
 // on load function e.g. when the Collapsible button is clicked
 function onLoad(url){
-    if(typeof(url) != 'undefined'){
-        // set the SameSite attribute for the cookies
-        setSameSiteAttribute('None');
-        console.log('url ' + url);
-        const YTid = url.split("=")[url.split("=").length-1];
-        const embedURL = "//www.youtube.com/embed/" + YTid + "/";
-        document.getElementById('Details'+@@AUTOID@@).onclick= function() {
-               document.getElementById('Content'+@@AUTOID@@).src = ""+embedURL;
-               document.getElementById('ShareLink'+@@AUTOID@@).href = ""+url;
-        };
-    }
+  if(typeof(url) != 'undefined'){
+    const YTid = url.split("=")[url.split("=").length-1];
+    const embedURL = "//www.youtube.com/embed/" + YTid + "/";
+    document.getElementById('Details'+@@AUTOID@@).onclick= function() {
+      document.getElementById('Content'+@@AUTOID@@).src = ""+embedURL;
+      document.getElementById('ShareLink'+@@AUTOID@@).href = ""+url;
+      // set the SameSite attribute for the cookies
+      setSameSiteAttribute('None');
+      };
+  }
 }
 
 // event listener for the details element state change
@@ -59,71 +62,11 @@ details.addEventListener("toggle", (event) => {
       /* the element was toggled open */
       detailsButton.style.color = '#468ff4';
       detailsButton.style.backgroundColor = '#CCCCCC';
+      headerLink.style.display = 'none';
     } else {
       /* the element was toggled closed */
       detailsButton.style.backgroundColor = '';
       detailsButton.style.color = '';
+      headerLink.style.display = 'block';
     }
   });
-  
-  // hide exit fullscreen button
-  function hideFullscreenExitButton() {
-    exitFullscreenButton.style.display = 'none';
-  }
-  
-  // show exit fullscreen button
-  function showFullscreenExitButton() {
-    exitFullscreenButton.style.display = 'block';
-  }
-  
-  // Event listener for the fullscreen button
-  //fullscreenButton.addEventListener('click', enterFullscreen);
-  
-  // Function to enter fullscreen
-  function enterFullscreen() {
-    if (fullscreenIframeContainer.requestFullscreen) {
-      fullscreenIframeContainer.requestFullscreen();
-    } else if (fullscreenIframeContainer.mozRequestFullScreen) {
-      fullscreenIframeContainer.mozRequestFullScreen();
-    } else if (fullscreenIframeContainer.webkitRequestFullscreen) {
-      fullscreenIframeContainer.webkitRequestFullscreen();
-    } else if (fullscreenIframeContainer.msRequestFullscreen) {
-      fullscreenIframeContainer.msRequestFullscreen();
-    }
-  }
-  
-  // Function to exit fullscreen
-  function exitFullscreen() {
-    console.log('exitFullscreen');
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-  }
-  
-  // Listen for fullscreen change event
-  document.addEventListener('fullscreenchange', handleFullscreenChange);
-  document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
-  document.addEventListener('mozfullscreenchange', handleFullscreenChange);
-  document.addEventListener('MSFullscreenChange', handleFullscreenChange);
-  
-  // Handle the fullscreen change event to hide/show the exit fullscreen button
-  function handleFullscreenChange() {
-    if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
-      // If the iframe enters fullscreen
-      showFullscreenExitButton();
-    } else {
-      // If the iframe exits fullscreen
-      hideFullscreenExitButton();
-    }
-  }
-  
-/*   // Event listener for the exit fullscreen button
-  exitFullscreenButton.addEventListener('click', () => {
-    exitFullscreen();
-  }); */
