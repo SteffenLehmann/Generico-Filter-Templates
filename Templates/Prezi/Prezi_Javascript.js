6585a2ec5c12734debf5501b3c06616f6203a5c7
SteffenLehmann <slehma15@student.aau.dk>
2023-08-01 13:19:47 +0200

    Add header links to prezi

--- Templates/Prezi/Prezi_Javascript.js
+++ Templates/Prezi/Prezi_Javascript.js
@@ -4,16 +4,19 @@ const nameForSummary = '@@Name: The name of the button containing the Prezi pres
 const preziURL = '@@Prezi URL: From your Prezi dashboard find the presentation you would like to share. Click the three dots in its thumbnail and select Sharing and privacy from the list. Then you can copy the link.@@';
 
 // getting the elements from the HTML
-const fullscreenIframeContainer = document.getElementById('iframeContainer'); 
-//const fullscreenButton = document.getElementById('fullscreenButton');
-//const exitFullscreenButton = document.getElementById('exitFullscreenButton');
 const details = document.getElementById('Details'+@@AUTOID@@);
 const detailsButton = document.getElementById('detailsButton');
+const headerLink = document.getElementById('ShareLinkHeader'+@@AUTOID@@);
 
 // function calls to create the temlate on moodle
 createNameForSummary(nameForSummary);
-console.log("before onload is called "+preziURL);
+assignHeaderLinks(preziURL);
 onLoad(preziURL);
+
+// function to assign the header links
+function assignHeaderLinks(url) {
+  headerLink.href = ""+ url;
+}
 
 //creates the name for the template
 function createNameForSummary(name) {
@@ -57,71 +60,11 @@ details.addEventListener("toggle", (event) => {
       /* the element was toggled open */
       detailsButton.style.color = '#468ff4';
       detailsButton.style.backgroundColor = '#CCCCCC';
+      headerLink.style.display = 'none';
     } else {
       /* the element was toggled closed */
       detailsButton.style.backgroundColor = '';
       detailsButton.style.color = '';
+      headerLink.style.display = 'block';
     }
   });
-  
-  // hide exit fullscreen button
-  function hideFullscreenExitButton() {
-    exitFullscreenButton.style.display = 'none';
-  }
-  
-  // show exit fullscreen button
-  function showFullscreenExitButton() {
-    exitFullscreenButton.style.display = 'block';
-  }
-  
-/*   // Event listener for the fullscreen button
-  fullscreenButton.addEventListener('click', enterFullscreen); */
-  
-  // Function to enter fullscreen
-  function enterFullscreen() {
-    if (fullscreenIframeContainer.requestFullscreen) {
-      fullscreenIframeContainer.requestFullscreen();
-    } else if (fullscreenIframeContainer.mozRequestFullScreen) {
-      fullscreenIframeContainer.mozRequestFullScreen();
-    } else if (fullscreenIframeContainer.webkitRequestFullscreen) {
-      fullscreenIframeContainer.webkitRequestFullscreen();
-    } else if (fullscreenIframeContainer.msRequestFullscreen) {
-      fullscreenIframeContainer.msRequestFullscreen();
-    }
-  }
-  
-  // Function to exit fullscreen
-  function exitFullscreen() {
-    console.log('exitFullscreen');
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-  }
-  
-  // Listen for fullscreen change event
-  document.addEventListener('fullscreenchange', handleFullscreenChange);
-  document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
-  document.addEventListener('mozfullscreenchange', handleFullscreenChange);
-  document.addEventListener('MSFullscreenChange', handleFullscreenChange);
-  
-  // Handle the fullscreen change event to hide/show the exit fullscreen button
-  function handleFullscreenChange() {
-    if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
-      // If the iframe enters fullscreen
-      showFullscreenExitButton();
-    } else {
-      // If the iframe exits fullscreen
-      hideFullscreenExitButton();
-    }
-  }
-  
-  // Event listener for the exit fullscreen button
-/*   exitFullscreenButton.addEventListener('click', () => {
-    exitFullscreen();
-  }); */
